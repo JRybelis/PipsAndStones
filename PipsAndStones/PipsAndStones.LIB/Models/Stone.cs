6f461cb630cb87dde7ef9edd928fa23b1d225f01
JRybelis <frizjr@protonmail.com>
2025-01-12 19:04:26 +0200

    Update Stone side validation

--- PipsAndStones/PipsAndStones.LIB/Models/Stone.cs
+++ PipsAndStones/PipsAndStones.LIB/Models/Stone.cs
@@ -5,12 +5,25 @@ public record Stone()
     private readonly int _firstSide;
     private readonly int _secondSide;
 
+    private const int MaxPipValue = 6;
+    private const int MinPipValue = 0;
+
     public Stone(int firstSide, int secondSide) : this()
     {
+        ValidatePipValue(firstSide, nameof(firstSide));
+        ValidatePipValue(secondSide, nameof(secondSide));
         _firstSide = firstSide;
         _secondSide = secondSide;
     }
-    
+
+    private static void ValidatePipValue(int value, string paramName)
+    {
+        if (value is < MinPipValue or > MaxPipValue)
+        {
+            throw new ArgumentException($"Pip values must be between {MinPipValue} and {MaxPipValue}", paramName);
+        }
+    }
+
     public int GetFirstSide() => _firstSide;
     public int GetSecondSide() => _secondSide;
 
