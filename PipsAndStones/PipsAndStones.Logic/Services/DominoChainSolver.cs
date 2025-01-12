2ef52421fa7687635eaa4f5194147d5ae33b2540
JRybelis <frizjr@protonmail.com>
2025-01-12 20:53:23 +0200

    Implement interface and add DominoNode

--- PipsAndStones/PipsAndStones.Logic/Services/DominoChainSolver.cs
+++ PipsAndStones/PipsAndStones.Logic/Services/DominoChainSolver.cs
@@ -1,6 +1,26 @@
+using PipsAndStones.LIB.Interfaces.Services;
+using PipsAndStones.LIB.Models;
+
 namespace PipsAndStones.Logic.Services;
 
-public class DominoChainSolver
+public class DominoChainSolver : IDominoChainSolver
 {
+    private class DominoNode
+    {
+        private readonly Stone _stone;
+        public DominoNode? Next { get; set; }
+
+        public DominoNode(Stone stone)
+        {
+            _stone = stone;
+        }
+        
+        public Stone GetStone() => _stone;
+    }
     
+    public Result<IEnumerable<Stone>> SolveChain(IEnumerable<Stone> stones)
+    {
+        throw new NotImplementedException();
+    }
 }
+
